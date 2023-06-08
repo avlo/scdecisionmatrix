669ce6572f7593bcf435d184f40583a3e15bb9c5
nick avlo <avlo@yahoo.com>
2023-06-09 07:17:18 +0800

    table name customizations and ongoing refactoring

--- src/main/java/com/prosilion/scdecisionmatrix/entity/AppUserAuthUser.java
+++ src/main/java/com/prosilion/scdecisionmatrix/entity/AppUserAuthUser.java
@@ -1,17 +1,19 @@
 package com.prosilion.scdecisionmatrix.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+
 import java.io.Serializable;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
 
+/**
+ * Join table between AuthDetailsUser (used for auth & auth) and AppUser (used for
+ * application specific business logic.
+ */
 @Data
 @NoArgsConstructor
-@Entity
+@Entity(name = "appuser_authuser")
 public class AppUserAuthUser implements Serializable {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
