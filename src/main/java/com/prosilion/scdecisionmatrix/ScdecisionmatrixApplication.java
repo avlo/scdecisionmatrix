b47848d0445cba0634c2ccf82fdeb8e138330223
nick avlo <avlo@yahoo.com>
2024-02-03 23:54:25 -0700

    war functions properly

--- src/main/java/com/prosilion/scdecisionmatrix/ScdecisionmatrixApplication.java
+++ src/main/java/com/prosilion/scdecisionmatrix/ScdecisionmatrixApplication.java
@@ -2,9 +2,15 @@ package com.prosilion.scdecisionmatrix;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ScdecisionmatrixApplication {
+public class ScdecisionmatrixApplication extends SpringBootServletInitializer {
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+    return builder.sources(ScdecisionmatrixApplication.class);
+  }
   public static void main(String[] args) {
     SpringApplication.run(ScdecisionmatrixApplication.class, args);
   }
--- src/test/resources/data.sql
+++
@@ -1,8 +0,0 @@
-insert into appuser (id)
-values (1);
-
-insert into appuser_authuser (id, app_user_id, auth_user_name)
-values (1, 1, "user");
-
-insert into contract (id, creator_role, app_user_id, counter_party_id, text)
-values (1, 0, 1, 1, "11111");
