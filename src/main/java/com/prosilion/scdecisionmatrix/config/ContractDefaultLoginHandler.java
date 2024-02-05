1dcc49f5ed3daeedacfd13079c0debdd956f357c
nick avlo <avlo@yahoo.com>
2024-02-04 21:04:25 -0700

    default success url

--- src/main/java/com/prosilion/scdecisionmatrix/config/ContractDefaultLoginHandler.java
+++ src/main/java/com/prosilion/scdecisionmatrix/config/ContractDefaultLoginHandler.java
@@ -2,16 +2,19 @@ package com.prosilion.scdecisionmatrix.config;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.DefaultRedirectStrategy;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 import java.io.IOException;
 
+// TODO: refactor below + application.properties key=value into cad3 framework
 public class ContractDefaultLoginHandler implements AuthenticationSuccessHandler {
-  private final static String SUCCESS_URL = "/contract/display_all";
+  @Value("${scd.login.url.successful}")
+  private String successUrl;
   @Override
   public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
-    new DefaultRedirectStrategy().sendRedirect(request, response, SUCCESS_URL);
+    new DefaultRedirectStrategy().sendRedirect(request, response, successUrl);
   }
 }
